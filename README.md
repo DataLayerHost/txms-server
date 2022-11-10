a39b966211334768b8c0a9b3a8030531f964e05a
Rastislav <rastislav@coreblockchain.cc>
2022-11-10 21:00:05 +0100

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,69 @@
-# txms-server
-TXMS server
+# TXMS server
+
+You can deploy this server to create your own TXMS webhook.
+
+## Installation
+
+Node.js 14 or newer is expected.
+
+### Creation of environment variables
+
+To start development version create `.env` file in the root of the project.
+
+Contents of `.env` file:
+
+```sh
+DEBUG=…
+PROVIDER=…
+ENDPOINT=…
+```
+
+Where:
+- DEBUG: Debugging of the program - 1/true / 0/false
+- PROVIDER: Url of the provider used (Currently [Blockbook](https://github.com/cryptohub-digital/blockbook) supported.)
+- ENDPOINT: Endpoint for streaming the Core Transactions
+
+### Installation of dependencies
+
+To install the dependencies, run the command:
+
+`npm i`
+
+## Development
+
+To run the development version, run command:
+
+`npm run start`
+
+## Deployment
+
+To deploy your project on the server, make sure you have correct rights and then follow:
+
+1. Git clone the repository
+1. Install dependencies
+1. Setup your environment variables
+1. Setup [PM2](https://pm2.keymetrics.io/) daemon process manager
+
+Example of the process:
+
+`pm2 start stream.js`
+
+## Expected stream
+
+We have expectations to get Json stream, for example:
+
+```json
+{
+  "from": "+123456789",
+  "to": "+987654321",
+  "body": "Hello world"
+}
+```
+
+## Epigram
+
+> 「Cryptoni Confidimus」
+
+## License
+
+Licensed under the [CORE License](LICENSE).
---
+++ stream.js
@@ -0,0 +1,95 @@
+require('dotenv').config()
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+app.use(bodyParser.json())
+
+app.get('/ping', (req, res) => {
+  res.sendStatus(200)
+}
+
+app.post('/stream', (req, res) => {
+  const debug = (process.env.DEBUG == 1 || process.env.DEBUG == true) ? true : false
+
+  const body = req.body.body
+  const number = req.body.from
+  if(typeof body === 'string' && body.trim().length === 0) {
+    let error = 'Err(1): Empty message'
+    let emptyerror = { "id": null, "message": error, "sent": false, "error": "Empty message", "errno": 1, "date": timestamp() }
+    if(debug) console.error('Err(1)', emptyerror)
+    return callback(JSON.stringify(emptyerror), null, res, 422)
+  }
+  const provider = (process.env.PROVIDER.endsWith('/') ? process.env.PROVIDER : process.env.PROVIDER + '/') + process.env.ENDPOINT
+
+  const txms = require('txms.js')
+  const axios = require('axios')
+
+  const parts = body.split(/\u000a/u)
+
+  parts.forEach(function (msg, index) {
+    let rawmsg = msg.trim()
+    const hextest = /^(0[xX])?[0-9a-fA-F]+$/
+    let hextx = ''
+    if (hextest.test(rawmsg)) {
+      if (rawmsg.toLowerCase().startsWith('0x')) {
+        hextx = rawmsg
+      } else {
+        hextx = '0x' + rawmsg
+      }
+      if(debug) console.log('Info', 'HEX message: ' + hextx)
+    } else if(typeof rawmsg === 'string' && rawmsg.length !== 0) {
+      hextx = txms.decode(rawmsg)
+      if(debug) console.log('Info', 'TXMS message: ' + hextx)
+    } else {
+      let error = 'Err(2): Empty message part'
+      let perror = { "id": null, "message": error, "sent": false, "error": "Empty message part", "errno": 2, "date": timestamp() }
+      if(debug) console.error('Err(2)', perror)
+      return callback(JSON.stringify(perror), null, res, 422)
+    }
+
+    axios.post(provider, hextx, {
+      headers: {
+        'Content-Type': 'text/plain',
+        'User-Agent': 'txms'
+      }
+    })
+    .then(function (response) {
+      if(response.result) {
+        let ok = 'OK: <'+hextx.substring(2, 5)+hextx.slice(-3)+'> '+response.result
+        let oks = { "message": ok, "sent": true, "hash": response.result, "date": timestamp() }
+        if(debug) console.log('OK', oks)
+        return callback(null, JSON.stringify(oks), res, 200)
+      } else {
+        let ok = 'OK: <'+hextx.substring(2, 5)+hextx.slice(-3)+'>'
+        let oks = { "message": ok, "sent": true, "date": timestamp() }
+        if(debug) console.log('OK', oks)
+        return callback(null, JSON.stringify(oks), res, 200)
+      }
+    })
+    .catch(function (err) {
+      let error = 'Err(3): <'+hextx.substring(2, 5)+hextx.slice(-3)+'>'
+      let errors = { "message": error, "sent": false, "error": err.message, "errno": 3, "date": timestamp() }
+      if(debug) console.error('Err(4)', errors)
+      return callback(JSON.stringify(errors), null, res, 500)
+    })
+  })
+
+})
+
+function callback(error, ok, res, code) {
+  if (ok) {
+    res.sendStatus((code ? code : 200)).send(ok)
+  } else if (error) {
+    res.sendStatus((code ? code : 500)).send(error)
+  } else {
+    res.sendStatus(500)
+  }
+}
+
+function timestamp() {
+  return new Date().toISOString()
+}
+
+const port = 8080
+app.listen(port, () => console.log(`App running on port ${port}`))
